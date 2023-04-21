f305cd1c888896797785936d382014d8184bdeb0
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-04-22 02:02:55 +0330

    color

--- components/name.tsx
+++ components/name.tsx
@@ -5,7 +5,6 @@ type NameCardProps = {
   export function NameCard({ name }: NameCardProps) {
     
     return (
-        <h1 className='uppercase bg-gradient-to-r from-teal-500 via-teal-600 to-teal-700 bg-clip-text text-transparent'>{name}</h1>
+        <h1 className='text-6xl	font-black uppercase bg-gradient-to-r from-teal-500 via-teal-600 to-indigo-600 bg-clip-text text-transparent'>{name}</h1>
        );
   }
-  
