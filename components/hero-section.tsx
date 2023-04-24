75acd78e22f722557978edde104e2441c0a6afcd
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-04-24 03:37:15 +0330

    card

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -60,9 +60,7 @@ export function HeroSection() {
         onTypingDone={() => dispatch({ type: 'setDone', payload: 'title' })}
       >
          <h1 className='text-9xl font-black uppercase bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 bg-clip-text text-transparent'>
-          <Balancer>
           Rose
-          </Balancer>
           </h1>
       </Typist>
       <h2 className="text-center min-h-[20vh] text-5xl font-black uppercase bg-gradient-to-r from-rose-500 via-rose-600 to-rose-700 bg-clip-text text-transparent">
