0fe524e3c9f13917c423bd008a09bda3a9cbccf7
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 03:18:09 +0330

    minor change

--- components/books.tsx
+++ components/books.tsx
@@ -7,7 +7,7 @@ type BooksProps = {
   export function Books() {
     
     return (
-        <div className="flex books-animation h-screen justify-around items-center">
+        <div className="flex bg-gradient-to-r from-amber-500 via-orange-500 to-orange-600 h-screen w-full justify-around items-center">
     </div>
         );
   }
