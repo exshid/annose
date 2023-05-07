829b0eee89ed8cedd74619c67c1a2938077fd8a3
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 20:11:46 +0330

    IntroSection

--- components/intro-section.tsx
+++ components/intro-section.tsx
@@ -8,7 +8,7 @@ type IntroSectionProps = {
   export function IntroSection() {
     
     return (
-        <div className="flex h-screen w-full bg-white dark:bg-lightdarkish justify-around items-center">
+        <div className="flex flex-col h-screen w-full bg-white dark:bg-lightdarkish justify-around items-center">
                <h3 className="jakarta-font text-5xl font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">More about A. B. Cdef</h3>
           <p className="jakarta-font text-darkish dark:text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. In, perferendis ut iure blanditiis magnam officia minima reprehenderit, illo commodi nobis soluta et delectus temporibus autem culpa, neque porro harum. Suscipit! Lorem ipsum dolor sit amet.
