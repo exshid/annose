3824e7b3b6d95296a0fff1835145ff10e5767f51
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 15:44:46 +0330

    img

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -7,14 +7,14 @@ type BookCardProps = {
   export function BookCard() {
     
     return (
-        <div className="group relative flex bg-white custom-boxshadow howver:shadow-none transition dark:shadow-none dark:bg-lightdarkish rounded-lg h-[550px] w-[350px] justify-around items-center">
+        <div className="group relative flex bg-white custom-boxshadow hover:shadow-none transition dark:shadow-none dark:bg-lightdarkish rounded-lg h-[550px] w-[350px] justify-around items-center">
  <Image 
           src={cover}
           alt="Cover of the book"
           height={550}
           width={350}
         className="!relative h-full object-cover rounded-lg w-full p-5"/>
-    <div className="flex justify-center items-center absolute bg-white dark:bg-lightdarkish h-1/6 w-full bottom-0 group-hover:delay-1000 group-hover:opacity-0 transition">
+    <div className="flex justify-center items-center absolute bg-white rounded-b-lg dark:bg-lightdarkish h-1/6 w-full bottom-0 group-hover:delay-1000 group-hover:opacity-0 transition">
       <h3 className="text-xl dark:text-white">Book Name</h3></div>
     </div>
 
