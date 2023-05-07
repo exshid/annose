07ec2e00ae4a9aef71f68eb7b213b0cf341de7bc
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 20:06:33 +0330

    IntroSection

--- components/intro-section.tsx
+++ components/intro-section.tsx
@@ -9,9 +9,7 @@ type IntroSectionProps = {
     
     return (
         <div className="flex h-screen w-full bg-white dark:bg-lightdarkish justify-around items-center">
-                     <h2 className='hero-font font-black uppercase bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent'>
-          A. B. CDEF
-          </h2>
+               <h3 className="jakarta-font text-5xl font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">More about A. B. Cdef</h3>
           <p className="jakarta-font text-darkish dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
     </div>
         );
