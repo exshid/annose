5fe13bbdbb7c1ff893327566e39b13295a82a639
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 03:31:24 +0330

    minor change

---
+++ components/bookcard.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import placeholder from '../public/images/placeholder.svg'
+type BookCardProps = {
+    photo: string;
+  };
+  
+  export function BookCard() {
+    
+    return (
+        <div className="flex bg-white dark:bg-darkish h-[200px] w-2/5 justify-around items-center">
+    </div>
+        );
+  }
--- components/books.tsx
+++ components/books.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import {BookCard} from '@/components/bookcard';
 import placeholder from '../public/images/placeholder.svg'
 type BooksProps = {
     photo: string;
@@ -8,6 +9,7 @@ type BooksProps = {
     
     return (
         <div className="flex bg-gradient-to-r from-emerald-400 to-cyan-400 h-screen w-full justify-around items-center">
+    <BookCard/>
     </div>
         );
   }
