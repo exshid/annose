b79dfc35313a5846b37bc2fddba98b9e0f4ff935
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-07 19:19:31 +0330

    padding

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -49,8 +49,7 @@ export function HeroSection() {
   return (
     <div
       className={cn(
-        'flex flex-col-reverse items-start h-full w-1/2 justify-center space-y-2',
-        'relative px-4',
+        'flex flex-col-reverse items-start h-full w-1/2 justify-center px-28 relative',
       )}
     >
       <Typist
