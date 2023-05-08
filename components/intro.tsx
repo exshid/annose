07702a6dc4a9192d8f8f8777335a11b30a460a9c
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 17:13:17 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,13 @@ type IntroProps = {
     
     return (
 <div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 dark:bg-gray-900 dark:text-gray-100">
-	<img src="https://source.unsplash.com/150x150/?portrait?3" alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
+<Image
+  src="https://source.unsplash.com/150x150/?portrait?3"
+  alt="picture of the author"
+  width={150}
+  height={150}
+  className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square"
+/>
 	<div className="space-y-4 text-center divide-y divide-gray-700">
 		<div className="my-2 space-y-1">
 			<h2 className="text-xl font-semibold sm:text-2xl">Leroy Jenkins</h2>
