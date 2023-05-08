acd55708cd6842a70a9ecc1a1aef2b70f704de18
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 16:14:20 +0330

    new section

--- components/service.tsx
+++ components/service.tsx
@@ -8,7 +8,7 @@ type ServiceProps = {
         <div className="bg-darkerlight py-5 dark:bg-darkish text-darkish dark:text-white flex justify-center h-auto w-full">
         <div className="w-8/12 flex flex-col items-center">
      
-    <h3 className="font-inter text-5xl p-5 font-black uppercase bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text text-transparent">
+    <h3 className="jakarta-title text-5xl p-5 font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
         Service for Authors</h3>
 <ul className="jakarta-title pt-5 pb-10 grid grid-cols-3 grid-rows-3 gap-8">
     <li className="s-boxshadow dark:shadow-none p-5 h-80 w-80 bg-white dark:bg-lightdarkish"><h4 className="p-5 font-bold text-xl">Novel editing</h4><p className="p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptas saepe vitae enim culpa ut assumenda accusamus distinctio laudantium debitis!</p></li>
