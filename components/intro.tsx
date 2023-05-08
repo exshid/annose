e2dea36118bde2fa2783409131608db0d5cc9f6b
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 16:37:31 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import paragraph from '../public/images/paragraph.svg'
+import placeholder from '../public/images/placeholder.svg'
 import { HeroSection } from '@/components/hero-section';
 type IntroProps = {
     photo: string;
@@ -10,7 +10,12 @@ type IntroProps = {
     return (
         <div className="bg-darkerlight dark:bg-lightdarkish text-darkish dark:text-white flex justify-center h-screen w-full">
         <div className="w-8/12 flex">
-     
+        <div className="w-1/2 justify-center items-center">
+        <Image 
+          src={placeholder}
+          alt="Picture of the author"
+        className="!relative h-full object-cover w-11/12 pt-5"/>
+</div>     
  <div className="flex flex-col w-1/2 justify-center items-center">
     <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent">
         About A. B. CDEF</h3>
