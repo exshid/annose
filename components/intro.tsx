59e70597c1cde49bc78394febf667341f49586e9
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 17:53:13 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,8 +8,8 @@ type IntroProps = {
   export function Intro() {
     
     return (
-      <div className="flex flex-col h-screen bg-darkerlight dark:bg-lightdarkish">
-          <h3 className="font-inter text-5xl pt-10 font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
+      <div className="flex flex-col h-screen bg-darkerlight dark:bg-lightdarkish justify-center items-center">
+          <h3 className="font-inter text-5xl pb-10 font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
         About A. B. CDEF</h3>
 
       <div className="w-full flex justify-around p-10">
@@ -23,7 +23,8 @@ type IntroProps = {
 />
 </div>
 <div className="flex flex-col w-2/4 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
-<p className="jakarta-font text-darkish dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, ad asperiores ipsam voluptatem rem a id maxime odio ullam accusantium dolor magnam quam expedita! A tenetur ipsam enim magnam cupiditate.</p>
+<p className="jakarta-font text-darkish dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, ad asperiores ipsam voluptatem rem a id maxime odio ullam accusantium dolor magnam quam expedita! A tenetur ipsam enim magnam cupiditate.
+Quos iure in voluptas! Libero aspernatur error temporibus id aperiam culpa similique pariatur ex, optio sunt. Pariatur velit fugiat consequuntur quaerat eius.</p>
 </div>
 </div>
 </div>
