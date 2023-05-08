fde57d9a687c883d2463db701eecc41ed8405d66
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 15:28:23 +0330

    new section

--- components/service.tsx
+++ components/service.tsx
@@ -10,8 +10,6 @@ type ServiceProps = {
      
     <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text text-transparent">
         Service for Authors</h3>
-<ul className="flex flex-wrap">
-
 <ul className="grid grid-cols-3 grid-rows-3 gap-5">
     <li className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500">1</li>
     <li className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 col-start-3 row-start-3">2</li>
@@ -24,51 +22,6 @@ type ServiceProps = {
     <li className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 col-start-1 row-start-2">9</li>
 </ul>
     
-
-
-
-
-
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-    <li className="w-1/3 h-64 text-white my-5">
-        <div className="bg-gradient-to-r h-full from-cyan-400 to-cyan-500 m-5">
-<h4>Novel editing</h4>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
-    </div>
-    </li>
-
-</ul>
  </div>
  </div>
  
