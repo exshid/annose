1e648c0ba0fe0964b26742cdede33f619ec9c9ba
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 18:11:23 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,10 +9,10 @@ type IntroProps = {
     
     return (
       <div className="flex flex-col h-screen bg-darkerlight dark:bg-lightdarkish justify-center items-center">
-          <h3 className="font-inter text-5xl pb-10 font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
+          <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
         Abou A. B. CDEF</h3>
 
-      <div className="w-full flex justify-around p-10">
+      <div className="w-full flex justify-evenly p-10">
 <div className="flex flex-col w-2/6 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
 <Image
   src={placeholder}
