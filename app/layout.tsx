ac5923f7b58da42dc091aa2315c35e7c35cbcb21
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-09 01:18:09 +0330

    animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import 'react-tooltip/dist/react-tooltip.css';
 import { type Metadata } from 'next/types';
 import { allPosts } from 'contentlayer/generated';
-import { motion, useScroll, useSpring } from "framer-motion";
 import { blogConfig } from '@/config';
 import { Analytics } from '@/components/analytics';
 import { Footer } from '@/components/footer';
@@ -58,13 +57,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  });
-
 
   return (
     <html
@@ -76,7 +68,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body className="min-h-screen w-full bg-white dark:bg-darkish">
         <>
-      <motion.div className="progress-bar" style={{ scaleX }} />
 
         <section className="fixed top-0 z-30 w-11/12 sm:w-full">
             <Header />
