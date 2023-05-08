96eac04a75a3106ce15004523034e3a433aa89eb
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 17:46:53 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,17 +8,17 @@ type IntroProps = {
   export function Intro() {
     
     return (
-      <div className="w-full flex p-10 bg-darkerlight dark:bg-lightdarkish">
-<div className="flex flex-col w-1/3 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 shadow-md rounded-xl sm:px-12 dark:text-gray-100">
+      <div className="w-full flex justify-around p-10 bg-darkerlight dark:bg-lightdarkish">
+<div className="flex flex-col w-1/4 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
 <Image
   src={placeholder}
   alt="picture of the author"
   width={450}
   height={450}
-  className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square"
+  className="rounded-full dark:bg-gray-500 aspect-square"
 />
 </div>
-<div className="flex flex-col w-2/3 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 shadow-md rounded-xl sm:px-12 dark:text-gray-100">
+<div className="flex flex-col w-2/4 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
 <p className="jakarta-font text-darkish dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, ad asperiores ipsam voluptatem rem a id maxime odio ullam accusantium dolor magnam quam expedita! A tenetur ipsam enim magnam cupiditate.</p>
 </div>
 
