41b67d21c77186b41673c132864d35dbe102215d
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 18:12:50 +0330

    new section

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,10 +10,10 @@ type IntroProps = {
     return (
       <div className="flex flex-col h-screen bg-darkerlight dark:bg-lightdarkish justify-center items-center">
           <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">
-        Abou A. B. CDEF</h3>
+        About A. B. CDEF</h3>
 
       <div className="w-full flex justify-evenly p-10">
-<div className="flex flex-col w-2/6 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
+<div className="flex flex-col w-2/6 justify-center p-6 sm:px-12 dark:text-gray-100">
 <Image
   src={placeholder}
   alt="picture of the author"
@@ -22,7 +22,7 @@ type IntroProps = {
   className="rounded-full dark:bg-gray-500 aspect-square"
 />
 </div>
-<div className="flex flex-col w-2/4 justify-center s-boxshadow dark:shadow-none bg-white dark:bg-lightdarkish p-6 sm:px-12 dark:text-gray-100">
+<div className="flex flex-col w-2/4 justify-center p-6 sm:px-12 dark:text-gray-100">
 <p className="jakarta-font text-darkish dark:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, ad asperiores ipsam voluptatem rem a id maxime odio ullam accusantium dolor magnam quam expedita! A tenetur ipsam enim magnam cupiditate.
 Quos iure in voluptas! Libero aspernatur error temporibus id aperiam culp similique pariatur ex, optio sunt. Pariatur velit fugiat consequuntur quaerat eius.</p>
 </div>
