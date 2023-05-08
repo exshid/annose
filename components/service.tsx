8e4405267c111a378e8ba8f24f97c1547dd38db4
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 15:41:05 +0330

    new section

--- components/service.tsx
+++ components/service.tsx
@@ -10,16 +10,16 @@ type ServiceProps = {
      
     <h3 className="font-inter text-5xl p-5 font-black uppercase bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text text-transparent">
         Service for Authors</h3>
-<ul className="grid grid-cols-3 grid-rows-3 gap-5">
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish">1</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-3">2</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-2">3</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-1">4</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-1">5</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-2">6</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-3">7</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-3">8</li>
-    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-2">9</li>
+<ul className="py-5 grid grid-cols-3 grid-rows-3 gap-5">
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish">1</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-3">2</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-2">3</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-1">4</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-1">5</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-2">6</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-3">7</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-3">8</li>
+    <li className="s-boxshadow h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-2">9</li>
 </ul>
     
  </div>
