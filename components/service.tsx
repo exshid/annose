c635e1dd56c822ff17bc8117c6094e35e5ac6bab
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 15:35:18 +0330

    new section

--- components/service.tsx
+++ components/service.tsx
@@ -5,21 +5,21 @@ type ServiceProps = {
   export function Service() {
     
     return (
-        <div className="bg-darkerlight dark:bg-darkish text-darkish dark:text-white flex justify-center h-screen w-full">
-        <div className="w-8/12 flex flex-col">
+        <div className="bg-darkerlight dark:bg-darkish text-darkish dark:text-white flex justify-center h-auto w-full">
+        <div className="w-8/12 flex flex-col items-center">
      
     <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text text-transparent">
         Service for Authors</h3>
 <ul className="grid grid-cols-3 grid-rows-3 gap-5">
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500">1</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-3 row-start-3">2</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-3 row-start-2">3</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-3 row-start-1">4</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-2 row-start-1">5</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-2 row-start-2">6</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-2 row-start-3">7</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-1 row-start-3">8</li>
-    <li className="bg-gradient-to-r h-80 from-cyan-400 to-cyan-500 col-start-1 row-start-2">9</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish">1</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-3">2</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-2">3</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-3 row-start-1">4</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-1">5</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-2">6</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-2 row-start-3">7</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-3">8</li>
+    <li className="h-80 w-80 bg-white dark:bg-lightdarkish col-start-1 row-start-2">9</li>
 </ul>
     
  </div>
