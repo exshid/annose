6f36d147d1fec08da8802b6e6b0b8b7c3127e876
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 23:09:52 +0330

    animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import 'react-tooltip/dist/react-tooltip.css';
 import { type Metadata } from 'next/types';
 import { allPosts } from 'contentlayer/generated';
-
+import { motion, useScroll, useSpring } from "framer-motion";
 import { blogConfig } from '@/config';
 import { Analytics } from '@/components/analytics';
 import { FontStyleProvider } from '@/components/font-style-provider';
@@ -59,6 +59,14 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+
   return (
     <html
       lang="en"
@@ -69,6 +77,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body className="min-h-screen w-full bg-white dark:bg-darkish">
         <FontStyleProvider>
+        <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+
         <section className="fixed top-0 z-30 w-11/12 sm:w-full">
             <Header />
           </section>
@@ -80,6 +91,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </section>
            <Search posts={allPosts} />
           <Analytics />
+        </>
         </FontStyleProvider>
       </body>
     </html>
