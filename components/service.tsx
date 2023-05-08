0ea860ca3e5518083374e110fe54d60f37396bf8
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-08 15:14:17 +0330

    new section

--- components/service.tsx
+++ components/service.tsx
@@ -8,32 +8,32 @@ type ServiceProps = {
         <div className="bg-darkerlight dark:bg-darkish text-darkish dark:text-white flex justify-center h-screen w-full">
         <div className="w-8/12 flex flex-col">
      
-    <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent">
+    <h3 className="font-inter text-5xl font-black uppercase bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text text-transparent">
         Service for Authors</h3>
 <ul className="flex flex-wrap">
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
 
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
 
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
-    <li className="ml-[50px] mb-[50px] bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 h-64 text-white">
+    <li className="bg-gradient-to-r from-cyan-400 to-cyan-500 w-1/4	h-64 text-white">
 <h4>Novel editing</h4>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempore inventore fugiat mlaceat nom odit delectus maxime nam temporibus distinctio.</p>
     </li>
