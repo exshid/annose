f3cc94f302de6970267d2ed16005a42e22ca92ac
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 02:17:03 +0330

    animation

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -20,28 +20,28 @@ type BookCardProps = {
           <div className="absolute inset-0 z-30  flex flex-col items-center justify-center backdrop-blur">
           <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <Image src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <Image alt="" src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <Image src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+    <Image alt="" src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <Image src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+    <Image alt="" src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <Image src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+    <Image alt="" src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
@@ -50,7 +50,7 @@ type BookCardProps = {
 </div>
 
               <div className="shadow-2xl rounded-lg overflow-hidden	flex w-[900px] h-[555px] bg-cover bg-center">
-      <Image src={cover} height={555} alt="cover of the book" width={355}/>
+      <Image alt="" src={cover} height={555} alt="cover of the book" width={355}/>
 <div className="h-full flex-col justify-evenly w-max p-5 flex text-white bg-gradient-to-r from-emerald-400 to-cyan-400"><h3 className="font-inter font-black text-5xl">Book Title</h3><p className="jakarta-font">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil fugit distinctio ut nemo corrupti. Aperiam, distinctio a obcaecati dolore ipsum, harum minus facere deleniti ullam cumque officiis ea minima!</p>
 <div>
     <button className="uppercase block w-full p-4 text-lg bg-white rounded-full focus:outline-none"><p className="uppercase 
