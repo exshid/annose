6f068577c14d9d9807ed92a9c6703cf8cbdbbbf1
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 02:33:11 +0330

    animation

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -12,29 +12,29 @@ type BookCardProps = {
     return (
 <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <Imgage alt="" src={placeholder} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    <Image alt="" src={placeholder} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 inset-x-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <Imgage alt="" src={cover} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    <Image alt="" src={cover} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 inset-x-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <Imgage alt="" src={placeholder} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    <Image alt="" src={placeholder} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 inset-x-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <Imgage alt="" src={cover} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    <Image alt="" src={cover} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 inset-x-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
