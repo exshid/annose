0a07fad9da70da136800f6beb5c9bee01dd2b6ca
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-09 20:38:21 +0330

    button

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,7 +33,8 @@ type ContactProps = {
                         <input className="block w-full p-4 text-lg border dark:border-white rounded-sm dark:bg-darkish" type="text" name="name" id="name" placeholder="Name" />
                     </div>
                     <div className="px-4 pb-2 pt-4">
-                        <button className="uppercase block w-full p-4 text-lg text-white rounded-full bg-gradient-to-r from-rose-600 to-rose-900 focus:outline-none">sign in</button>
+                        <button data-aos="fade-up" data-aos-once="true"
+     data-aos-anchor-placement="bottom-bottom" className="uppercase block w-full p-4 text-lg text-white rounded-full bg-gradient-to-r from-emerald-400 to-cyan-400 focus:outline-none">Send</button>
                     </div>
 
                     <div className="p-4 text-center inset-x-0 flex justify-center space-x-4 mt-16 lg:hidden ">
