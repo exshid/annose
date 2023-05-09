64f5b58b5a05c7f17992c3b5b125d5dacc9447e3
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-09 20:12:20 +0330

    button

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -58,15 +58,16 @@ export function HeroSection() {
         'flex flex-col-reverse items-start h-full w-3/5 justify-center px-28 relative',
       )}
     >
-<p data-aos="fade-up"  data-aos-once="true"
- className="jakarta-font text-darkish dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<p className="jakarta-font text-darkish dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <Typist
         typingDelay={0}
         splitter={splitter}
         pause={isPaused}
         onTypingDone={() => dispatch({ type: 'setDone', payload: 'title' })}
       >
-         <h1 className='hero-font font-black uppercase bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent'>
+         <h1 data-aos="fade-right"
+     data-aos-once="true"
+className='hero-font font-black uppercase bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent'>
           A. B. CDEF
           </h1>
       </Typist>
