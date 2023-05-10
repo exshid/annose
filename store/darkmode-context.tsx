83fc30c5bd857a95c8d01d8212a413d75c1dca2a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 23:24:36 +0330

    grid

--- store/darkmode-context.tsx
+++ store/darkmode-context.tsx
@@ -18,6 +18,7 @@ const ThemeContext = createContext<ThemeContextValue>({
 const ThemeContextProvider = ({ children, value }: ThemeContextProviderProps) => {
   const [isDark, setIsDark] = useState(false);
 
+  
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
       const darkMode = localStorage.getItem("theme");
