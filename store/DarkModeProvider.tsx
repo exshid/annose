7cc945878081ebea95cf2e1c07888aefb3993ca1
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 16:33:46 +0330

    dark mode

--- store/DarkModeProvider.tsx
+++ store/DarkModeProvider.tsx
@@ -1,10 +1,10 @@
+//@ts-nocheck 
 'use client'
 import React, { useState, useEffect } from 'react';
 import DarkMode from "./darkmode-context";
 
 
-
-const DarkModeProvider = (props<any>) => {
+const DarkModeProvider = (props:MyComponentProps) => {
 
     const [isDark, setIsDark] = useState(null);
     const [classes, setClasses] = useState('dark scroll-pt-16 overflow-auto overscroll-none jakarta-title');
