901db4c0bdc10493e185666fad8e3014eee72824
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 23:18:46 +0330

    grid

--- store/darkmode-context.tsx
+++ store/darkmode-context.tsx
@@ -1,32 +1,37 @@
 'use client';
 import { createContext, useContext, useEffect, useState } from "react";
-interface ThemeContextProviderProps {
+interface ThemeContextValue {
   isDark: boolean;
   setIsDark: React.Dispatch<React.SetStateAction<boolean>>;
-  children: React.ReactNode;
 }
 
-const ThemeContext = createContext<ThemeContextProviderProps>({
+interface ThemeContextProviderProps {
+  children: React.ReactNode;
+  value: ThemeContextValue;
+}
+
+const ThemeContext = createContext<ThemeContextValue>({
   isDark: false,
   setIsDark: () => {},
 });
 
+const ThemeContextProvider = ({ children, value }: ThemeContextProviderProps) => {
+  const [isDark, setIsDark] = useState(false);
 
-
-const ThemeContextProvider = ({ children }: ThemeContextProviderProps) => {
-    const [isDark, setIsDark] = useState(false);
-    useEffect(() => {
-      if (typeof window !== "undefined" && window.localStorage) {
-        const darkMode = localStorage.getItem("theme");
-        if (darkMode === 'dark') {
-          setIsDark(true)
-        }
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const darkMode = localStorage.getItem("theme");
+      if (darkMode === "dark") {
+        setIsDark(true);
       }
-    }, [isDark]);
-    return (
-        <ThemeContext.Provider value={{ isDark, setIsDark }}>
-            {children}
-        </ThemeContext.Provider>
-    )
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ isDark, setIsDark, ...value }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
+
 export const useThemeContext = () => useContext(ThemeContext);
