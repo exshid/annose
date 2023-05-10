7510ba18a2a4ff70f18bfa017b61cc641d81153d
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 20:32:12 +0330

    new

--- store/darkmode-context.tsx
+++ store/darkmode-context.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext({})
 interface ThemeContextProviderProps {
