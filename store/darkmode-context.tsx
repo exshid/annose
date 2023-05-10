558b654306d0acbe9796db1885ae8bd815c50cd4
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 16:46:02 +0330

    dark mode

--- store/darkmode-context.tsx
+++ store/darkmode-context.tsx
@@ -1,9 +1,12 @@
 'use client'
 import React from "react";
 
-const DarkMode = React.createContext({
-    darkmode: 'dark'
-})
+interface DarkModeContextValue {
+  classes: string;
+}
 
+const DarkMode = React.createContext<DarkModeContextValue>({
+  classes: "dark scroll-pt-16 overflow-auto overscroll-none jakarta-title",
+});
 
 export default DarkMode;
