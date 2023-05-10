bb5b0fe451c2403d3921912dbacded202dc667ef
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 17:37:14 +0330

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   const { isDark } = useContext(DarkMode);
+
   return (
     <DarkModeProvider>
 
