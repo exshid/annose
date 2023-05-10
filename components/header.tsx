25a426a041e5ba9ae3eed70b005b71e1a3a7c4ab
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-11 00:27:01 +0330

    grid

--- components/header.tsx
+++ components/header.tsx
@@ -33,16 +33,20 @@ export function Header() {
 
 <>
 
-<div className="navbar bg-base-100">
+<div       ref={headerRef}
+      className={cn(
+        headerRef.current && scrollTop > headerRef.current.clientHeight
+          ? 'border-b border-b-slate-300 text-darkish dark:text-white bg-white dark:bg-darkish py-2 dark:border-b-slate-600'
+          : 'py-8 bg-transparent ',
+        'flex flex-row items-center justify-between px-4 xs:px-8 text-darkish dark:text-white',
+        'transition-[padding,background-color] duration-300 ease-in-out navbar')}>
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
+      <NavigationBar className="flex-grow max-xs:mr-2" />
       </ul>
     </div>
   </div>
@@ -50,31 +54,10 @@ export function Header() {
     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
   </div>
   <div className="navbar-end">
-    <button className="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
-      </div>
-    </button>
+  <Toolbar fontControls={isPostPage} className="ml-auto" />
   </div>
 </div>
 
-    <nav
-      ref={headerRef}
-      className={cn(
-        headerRef.current && scrollTop > headerRef.current.clientHeight
-          ? 'border-b border-b-slate-300 text-darkish dark:text-white bg-white dark:bg-darkish py-2 dark:border-b-slate-600'
-          : 'py-8 bg-transparent ',
-        'flex flex-row items-center justify-between px-4 xs:px-8 text-darkish dark:text-white',
-        'transition-[padding,background-color] duration-300 ease-in-out'
-      )}
-    >
-      <NavigationBar className="flex-grow max-xs:mr-2" />
-      <Toolbar fontControls={isPostPage} className="ml-auto" />
-    </nav>
   </>
   );
 }
