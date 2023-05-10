ec63028cda5d3c852c5485b70c9582cb36960075
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-10 17:10:46 +0330

    dark mode

--- store/DarkModeProvider.tsx
+++ store/DarkModeProvider.tsx
@@ -3,25 +3,41 @@
 import React, { useState, useEffect } from 'react';
 import DarkMode from "./darkmode-context";
 
-const DarkModeProvider = (props) => {
-
-    const [isDark, setIsDark] = useState(null);
-    const [classes, setClasses] = useState('dark scroll-pt-16 overflow-auto overscroll-none jakarta-title');
-
-          const darkMode = localStorage.getItem("theme");
-    
-          setIsDark(darkMode);
-        console.log(darkMode);
-    
-
-        if (isDark === 'dark') {
-        setClasses('dark scroll-pt-16 overflow-auto overscroll-none jakarta-title')
-        }
-
-    return <DarkMode.Provider value={classes}>
-        {props.children}
-    </DarkMode.Provider>
+interface DarkModeProviderProps {
+  children: React.ReactNode;
 }
 
+const DarkModeProvider = ({ children }: DarkModeProviderProps) => {
+  const [isDark, setIsDark] = useState(null);
+  const [classes, setClasses] = useState('dark scroll-pt-16 overflow-auto overscroll-none jakarta-title');
 
-export default DarkModeProvider
+  useEffect(() => {
+    // check if localStorage is available
+    if (typeof window !== "undefined" && window.localStorage) {
+      // get data from localStorage
+      const darkMode = localStorage.getItem("theme");
+
+      // set state with data from localStorage
+      setIsDark(darkMode);
+      console.log(darkMode);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDark === 'dark') {
+      setClasses('dark scroll-pt-16 overflow-auto overscroll-none jakarta-title')
+    }
+  }, [isDark])
+
+  const darkContext = {
+    classes: classes,
+  }
+
+  return (
+    <DarkMode.Provider value={darkContext}>
+      {children}
+    </DarkMode.Provider>
+  );
+}
+
+export default DarkModeProvider;
