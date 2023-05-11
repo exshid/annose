42a94c07a0dcac4b6b4ebb070d956c434cda7c7c
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-11 04:15:22 +0330

    responsive

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,21 +3,21 @@
     
     return (
     <div>
-    <section className="min-h-screen flex flex-col md:flex-row items-stretch text-white ">
-        <div className="md:flex w-full md:w-1/2 bg-gradient-to-r from-emerald-400 to-cyan-400 relative items-center">
+    <section className="min-h-screen flex flex-col lg:flex-row items-stretch text-white ">
+        <div className="lg:flex w-full p-8 lg:w-1/2 bg-gradient-to-r from-emerald-400 to-cyan-400 relative items-center">
             <div className="absolute bg-darkish opacity-60 inset-0 z-0"></div>
-            <div className="w-full px-2 md:px-24 z-10">
+            <div className="w-full px-2 lg:px-24 z-10">
                 <h1 data-aos="fade-right" data-aos-once="true" className="text-5xl font-bold text-left tracking-wide">Get In Touch</h1>
-                <p data-aos="fade-right" data-aos-once="true" className="text-3xl my-4">Email me at<br/>email@example.com</p>
+                <p data-aos="fade-right" data-aos-once="true" className="text-xl lg:text-3xl my-4">Email me at<br/>email@example.com</p>
             </div>
 
         </div>
-        <div className="md:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0 bg-white dark:bg-darkish text-darkish dark:text-white">
+        <div className="lg:w-1/2 w-full flex items-center justify-center text-center lg:px-16 px-0 z-0 bg-white dark:bg-darkish text-darkish dark:text-white">
             <div className="w-full py-6 z-20">
          <p  data-aos="fade-up" data-aos-once="true" className="text-3xl my-4 text-darkish dark:text-white">
               SIGN UP FOR MY NEWSLETTER
                 </p>
-                <form data-aos="fade-up" data-aos-once="true" action="" className="sm:w-2/3 w-full px-4 md:px-0 mx-auto">
+                <form data-aos="fade-up" data-aos-once="true" action="" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
                     <div className="pb-2 pt-4">
                         <input type="email" name="email" id="email" placeholder="Email" className="dark:bg-darkish block w-full p-4 border dark:border-white text-lg rounded-sm"/>
                     </div>
