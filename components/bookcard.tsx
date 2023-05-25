6ce1dfdb76502ac066ee69a51b51d30a42000e3f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-25 16:54:53 +0330

    minor

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -11,13 +11,18 @@ type BookCardProps = {
   export function BookCard() {
     const covers = [cover, coverII];
     const [transitionClass, setTransitionClass] = useState("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-100 bg-center bg-fixed");
+    const [transitionClassImg, setTransitionClassImg] = useState("w-auto h-auto rounded-lg opacity-100 lg:rounded-none transition duration-500 delay-500 ease-in-out object-cover contain lg:w-[555px] lg:h-full");
+
     const [count, setCount] = useState(0);
     function handleClick() {
       setCount((count + 1) % covers.length);
       setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-0 bg-center bg-fixed");
+      setTransitionClass("w-auto h-auto rounded-lg lg:rounded-none transition duration-500 delay-500 opacity-0 ease-in-out object-cover contain lg:w-[555px] lg:h-full");
 
       setTimeout(() => {
-        setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-0 bg-center bg-fixed");
+        setTransitionClass("w-auto h-auto rounded-lg lg:rounded-none transition duration-500 delay-500 opacity-100 ease-in-out object-cover contain lg:w-[555px] lg:h-full");
+
+        setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-100 bg-center bg-fixed");
       }, 500);
   
     }
@@ -32,7 +37,7 @@ type BookCardProps = {
           <div className="absolute inset-0 z-20 flex items-center justify-center h-auto lg:h-screen w-full bg-gray-900 bg-opacity-75"></div>
           <div className="absolute inset-0 z-30  flex flex-col items-center justify-center backdrop-blur">
               <div data-aos="fade-right" data-aos-once="true" className="shadow-2xl flex-col lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-full h-full lg:w-[840px] lg:h-[555px] bg-cover bg-center">
-      <Image src={covers[count]} alt="cover of the book" className="w-auto h-auto rounded-lg lg:rounded-none transition duration-500 delay-500 ease-in-out object-cover contain lg:w-[555px] lg:h-full"/>
+      <Image src={covers[count]} alt="cover of the book" className={transitionClassImg}/>
 <div className="flex h-min lg:h-full bottom-[17px] lg:bottom-[unset] flex-col justify-evenly w-auto rounded-lg lg:rounded-none absolute lg:relative lg:w-max p-5 text-white bg-gradient-to-r from-emerald-400 to-cyan-400"><h3 className="font-inter font-black text-5xl">Book Title</h3>
 <p className="hidden lg:block jakarta-font !m-0 !lg:mb-[40px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil fugit distinctio ut nemo corrupti. Aperiam, distinctio a obcaecati dolore ipsum, harum minus facere deleniti ullam cumque officiis ea minima!</p>
 <div className="hidden lg:block">
