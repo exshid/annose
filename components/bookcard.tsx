2df2d54fc797b9fde3a149be9c3d51d2c6708b25
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-25 16:10:08 +0330

    minor

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -1,5 +1,5 @@
+'use client'
 import React, { useState } from 'react';
-
 import Image from 'next/image'
 import cover from '../public/images/cover.jpg'
 import coverII from '../public/images/cover-ii.jpg'
