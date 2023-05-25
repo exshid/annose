2bb1ce083ae5a1d8d5504a525e08d26def77c30a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-25 21:42:49 +0330

    minor

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -17,14 +17,14 @@ type BookCardProps = {
     function classNames() {
       setCount((count + 1) % covers.length);
       setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-0 bg-center bg-fixed");
-      setTransitionClassImg("w-auto h-auto rounded-lg lg:rounded-none transition opacity-0 ease-in-out object-cover contain lg:w-[555px] lg:h-full");
+      setTransitionClassImg("opacity-0 z-[55] shadow-2xl flex-col lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-full h-full lg:w-[840px] lg:h-[555px] bg-cover bg-center");
 
       setTimeout(() => {
 
         setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-100 bg-center bg-fixed");
       }, 500);
       setTimeout(() => {
-        setTransitionClassImg("w-auto h-auto rounded-lg lg:rounded-none transition opacity-100 ease-in-out object-cover contain lg:w-[555px] lg:h-full");
+        setTransitionClassImg("z-[55] shadow-2xl flex-col lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-full h-full lg:w-[840px] lg:h-[555px] bg-cover bg-center");
 
       }, 200);
   
@@ -49,8 +49,8 @@ type BookCardProps = {
           </div>
           <div className="absolute inset-0 z-20 flex items-center justify-center h-auto lg:h-screen w-full bg-gray-900 bg-opacity-75"></div>
           <div className="absolute inset-0 z-30 flex flex-col items-center justify-center backdrop-blur">
-              <div data-aos="fade-right" data-aos-once="true" className="shadow-2xl flex-col lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-full h-full lg:w-[840px] lg:h-[555px] bg-cover bg-center">
-      <Image src={covers[count]} alt="cover of the book" className={transitionClassImg}/>
+              <div data-aos="fade-right" data-aos-once="true" className={transitionClassImg}>
+      <Image src={covers[count]} alt="cover of the book" className="w-auto h-auto rounded-lg lg:rounded-none transition opacity-100 ease-in-out object-cover contain lg:w-[555px] lg:h-full"/>
 <div className="flex h-min lg:h-full bottom-[17px] lg:bottom-[unset] flex-col justify-evenly w-auto rounded-lg lg:rounded-none absolute lg:relative lg:w-max p-5 text-white bg-gradient-to-r from-emerald-400 to-cyan-400"><h3 className="font-inter font-black text-5xl">Book Title</h3>
 <p className="hidden lg:block jakarta-font !m-0 !lg:mb-[40px]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil fugit distinctio ut nemo corrupti. Aperiam, distinctio a obcaecati dolore ipsum, harum minus facere deleniti ullam cumque officiis ea minima!</p>
 <div className="hidden lg:block">
