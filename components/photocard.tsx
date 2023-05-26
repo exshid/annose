892105f2d7ac49e94141244ea19859d6e4aa535c
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-05-26 15:25:11 +0330

    buttons

--- components/photocard.tsx
+++ components/photocard.tsx
@@ -8,7 +8,7 @@ type PhotoCardProps = {
   export function PhotoCard() {
     
     return (
-        <div className="flex flex-col-reverse lg:flex-row h-auto min-h-screen justify-evenly lg:justify-around items-center">
+        <div className="flex flex-col-reverse lg:flex-row h-screen justify-evenly lg:justify-around items-center">
     <HeroSection/>
 <div data-aos="zoom-in" data-aos-once="true" className='h-auto lg:h-full w-3/5 lg:w-2/5 flex clip-custom justify-center bg-gradient-to-r from-emerald-400 to-cyan-400 pt-5'>
     <Image 
