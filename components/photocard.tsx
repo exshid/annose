4091229292b8887b89a134f41d3935641dcf5e61
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-04 17:16:58 +0330

    responsive

--- components/photocard.tsx
+++ components/photocard.tsx
@@ -8,9 +8,9 @@ type PhotoCardProps = {
   export function PhotoCard() {
     
     return (
-        <div className="flex flex-col-reverse lg:flex-row p-5 md:p-0 h-auto xl:h-screen justify-evenly lg:justify-around items-center">
+        <div className="flex flex-col-reverse lg:flex-row p-5 md:p-0 h-auto lg:h-screen justify-evenly lg:justify-around items-center">
     <HeroSection/>
-<div data-aos="zoom-in" data-aos-once="true" className='h-auto md:h-full w-3/5 lg:w-2/4 xl:w-2/5 flex clip-custom justify-center bg-gradient-to-r from-emerald-400 to-cyan-400 pt-5'>
+<div data-aos="zoom-in" data-aos-once="true" className='h-auto lg:h-full w-3/5 lg:w-2/4 xl:w-2/5 flex clip-custom justify-center bg-gradient-to-r from-emerald-400 to-cyan-400 pt-5'>
     <Image 
           src={placeholder}
           alt="Picture of the author"
