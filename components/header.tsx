0576f882fda118346d51add098263fe9e085028c
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-04 17:58:25 +0330

    responsive

--- components/header.tsx
+++ components/header.tsx
@@ -44,7 +44,7 @@ export function Header() {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow dark:bg-lightdarkish bg-darkerlight rounded-box w-auto">
+      <ul tabIndex={0} className="menu menu-compact !text-base dropdown-content mt-3 p-2 shadow dark:bg-lightdarkish bg-darkerlight rounded-box w-auto">
       <NavigationBar className="flex-grow flex-column h-auto max-xs:mr-2" />
     </ul>
     </div>
