4b349c097a09abb612aa9ae378319df7147da13a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-05 23:36:32 +0330

    responsive

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -2,9 +2,9 @@
   export function Testimonials() {
     
     return (
- <div className="h-screen bg-gradient-to-r from-fuchsia-700 via-fuchsia-800 to-fuchsia-900">
-<div className="box"><i className="fas fa-quote-left fa2" />
-  <div className="text"><i className="fas fa-quote-right fa1" />
+ <div className="h-screen bg-white dark:bg-lightdarkish">
+<div className="box">
+  <div className="text bg-gradient-to-r from-rose-600 to-rose-900">
     <div>
       <h3>Testimonials</h3>
       <p>Lorem, ipsum dolor si amet consectetur adipisicing elit. Dignissimos nihil fugit distinctio ut nemo corrupti. Aperiam, distinctio a obcaecati dolore ipsum, harum minus facere deleniti ullam cumque officiis ea minima!
