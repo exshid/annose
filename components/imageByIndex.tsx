77344a5664ba317b805744c607b99d8b4225f64f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-05 18:49:01 +0330

    responsive

--- components/imageByIndex.tsx
+++ components/imageByIndex.tsx
@@ -1,7 +1,7 @@
-import cover from '../public/images/cover.jpg'
-import coverII from '../public/images/cover-ii.jpg'
+import image1 from '../public/images/cover.jpg'
+import image2 from '../public/images/cover-ii.jpg'
 
-export const images: string[] = [cover, coverII]
+export const images: string[] = [image1, image2]
 
 const imageByIndex = (index: number): string => images[index % images.length]
 
