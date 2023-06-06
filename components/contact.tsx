9dee72ccf044a0e707ec2bbd9545d4a1b01f8372
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-06 22:36:59 +0330

    responsive

--- components/contact.tsx
+++ components/contact.tsx
@@ -30,7 +30,7 @@ import { blogConfig } from '@/config';
                         <input className="bg-white block w-full p-4 text-lg border dark:border-white rounded-sm dark:bg-darkish" type="text" name="name" id="name" placeholder="Name" />
                     </div>
                     <div className="pb-2 pt-4">
-                        <input className="contact-placeholder bg-white block w-full p-4 text-lg border min-h-[151px] dark:border-white rounded-sm dark:bg-darkish" type="text" name="message" id="message" placeholder="Your message..." />
+                        <textarea className="contact-placeholder bg-white block w-full p-4 text-lg border min-h-[151px] dark:border-white rounded-sm dark:bg-darkish" type="text" name="message" id="message" placeholder="Your message..." />
                     </div>
                     <div className="relative pb-2 pt-4">
                         <button className="uppercase block w-full p-4 text-lg text-white rounded bg-gradient-to-r from-emerald-400 to-cyan-400 focus:outline-none transition hover:-translate-y-[4px]">Send</button>
