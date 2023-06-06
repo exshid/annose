7d96f85a1c3c926aef3705fb74a7d34484da4e6f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-06 18:43:11 +0330

    ui

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import placeholder from '../../public/images/placeholder.svg'
-import Typist from 'react-typist-component';
+
 import { type Metadata } from 'next/types';
 import { allPosts } from 'contentlayer/generated';
 import { compareDesc } from 'date-fns';
@@ -36,7 +36,7 @@ export default function About() {
 
   return (
     <div className="flex h-full items-center flex-col w-full py-24 lg:pt-40">
-      <p className="font-black px-2 md:px-5 lg:px-10 mb-10 xl:px-80 text-5xl text-darkish dark:text-white">About Me</p>
+      <p className="font-black px-2 md:px-5 lg:px-10 mb-10 xl:px-80 text-3xl md:text-4xl lg:text-5xl text-darkish dark:text-white">About Me</p>
       <div className="relative lg:rounded-2xl mb-10 w-full lg:w-9/12 flex flex-col lg:flex-row justify-between my-2 bg-gradient-to-r from-rose-600 to-rose-900 h-[60vh]">
       <Image
   src={placeholder}
@@ -46,12 +46,7 @@ export default function About() {
   objectFit="contain"
   objectPosition="center"
   /></div>
-      <div className="w-full px-2 md:px-5 lg:px-10 xl:px-80 h-full pb-2 self-start">
-        <Typist>
-<p className="inline-block text-5xl mr-5 text-darkish font-black dark:text-white">Hi, I am </p>
-<h1 className="inline-block font-black text-5xl bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">A. B. CDEF</h1>
-</Typist>
-</div>
+      <div className="w-full px-2 md:px-5 lg:px-10 xl:px-80 h-full pb-2 self-start"><p className="inline-block text-3xl md:text-4xl lg:text-5xl mr-5 text-darkish font-black dark:text-white">Hi, I am </p><h1 className="inline-block font-black text-3xl md:text-4xl lg:text-5xl bg-gradient-to-r from-rose-600 to-rose-900 bg-clip-text text-transparent">A. B. CDEF</h1></div>
 <div className="px-2 md:px-5 lg:px-10 xl:px-80 w-full text-darkish items-center jakarta-font dark:text-white">
     <p className="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate voluptatum vel in! Ipsa laborum, ipsam excepturi maxime corrupti aperiam nam. Similique cum animi enim reiciendis.</p><p className="py-2">Quas eius aliquid tempora aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non temporibus laboriosam, ratione earum nisi debitis dignissimos quia consequuntur obcaecati! Expedita neque laboriosam eius dolore. Aliquid reprehenderit aut soluta nemo atque!
 </p><p className="py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab expedita rem dolorum tempora perspiciatis at id illo iure quaerat libero corrupti veritatis vel quibusdam minus aperiam, necessitatibus, consequuntur, nulla maxime! Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit.
