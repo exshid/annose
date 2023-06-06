2b9d80356bac32df749a100d7613f02cb198870d
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 00:39:23 +0330

    ui

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -49,7 +49,7 @@ type BookCardProps = {
           <div className="absolute inset-0 z-30 flex flex-col items-center justify-center backdrop-blur-md">
               <div data-aos="fade-right" data-aos-once="true" className="z-[55] flex-col lg:bg-gradient-to-r from-emerald-400 to-cyan-400 lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-11/12 h-4/5 lg:w-[840px] lg:h-[555px] bg-cover bg-center">
       <div className="w-full flex justify-center lg:w-2/5 h-full overflow-hidden">
-      <Image src={blogConfig.books[count].cover} alt="cover of the book" className={transitionClassImg}/></div>
+</div>
 <div className="flex h-min lg:h-full bottom-[17px] lg:bottom-[unset] flex-col justify-evenly w-auto rounded-lg lg:rounded-none absolute lg:relative lg:w-3/5 p-5 text-white bg-gradient-to-r lg:bg-none from-emerald-400 to-cyan-400"><div><h3 className="font-inter font-black text-3xl lg:text-5xl">{blogConfig.books[count].title}</h3><p className="jakarta-font">By {blogConfig.books[count].author}</p></div>
 <p className="hidden lg:block jakarta-font !m-0 !lg:mb-[40px]">{blogConfig.books[count].description}</p>
 </div>
