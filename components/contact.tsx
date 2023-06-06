29010126f2e06b73947fb2767dcede5c34baadc1
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-06 22:03:00 +0330

    responsive

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,15 +4,7 @@ import { blogConfig } from '@/config';
   
   export const Contact = () => {
     const { footerLinks } = blogConfig;
-    let emailSection;
-    if (typeof footerLinks !== "undefined") {
-      emailSection = (
-        <p data-aos="fade-right" data-aos-once="true" className="text-xl lg:text-3xl my-4">
-          Email me at<br/>{footerLinks.email}
-        </p>
-      );
-    }
-  
+    
     return (
     <div id="contact">
     <section className="min-h-screen flex flex-col lg:flex-row items-stretch text-white ">
@@ -20,10 +12,9 @@ import { blogConfig } from '@/config';
             <div className="absolute bg-darkish opacity-60 inset-0 z-0"></div>
             <div className="w-full px-2 xl:px-24 z-10">
                 <h1 data-aos="fade-right" data-aos-once="true" className="text-5xl font-bold text-left tracking-wide">Get In Touch</h1>
-               {emailSection}
 
-
-                </div>
+                <p data-aos="fade-right" data-aos-once="true" className="text-xl lg:text-3xl my-4">Email me at<br/>{footerLinks?.email && footerLinks.email}</p>
+            </div>
 
         </div>
         <div className="lg:w-1/2 w-full flex items-center justify-center text-center xl:px-16 px-0 z-0 bg-white dark:bg-darkish text-darkish dark:text-white">
