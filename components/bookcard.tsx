2f81219f82cbd094b9aad640ad48924509ded470
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 01:09:46 +0330

    ui

--- components/bookcard.tsx
+++ components/bookcard.tsx
@@ -9,6 +9,7 @@ type BookCardProps = {
   
   export function BookCard() {
     console.log(blogConfig.books);
+    const [transitionClassText, setTransitionClassText] = useState("opacity-100");
     
     const [transitionClass, setTransitionClass] = useState("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-100 bg-center bg-fixed");
     const [transitionClassImg, setTransitionClassImg] = useState("w-auto h-full lg:h-auto rounded-lg opacity-100 lg:rounded-none transition ease-in-out object-cover contain lg:h-full");
@@ -18,8 +19,10 @@ type BookCardProps = {
       setCount((count + 1) % blogConfig.books.length);
       setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-0 bg-center bg-fixed");
       setTransitionClassImg("w-auto h-full lg:h-auto rounded-lg lg:rounded-none transition opacity-0 ease-in-out object-cover contain lg:h-full");
+      const [transitionClassText, setTransitionClassText] = useState("opacity-0");
 
       setTimeout(() => {
+        const [transitionClassText, setTransitionClassText] = useState("opacity-100");
 
         setTransitionClass("transition ease-in-out inset-0 h-screen bg-cover transition ease-in-out opacity-100 bg-center bg-fixed");
       }, 500);
@@ -54,8 +57,8 @@ type BookCardProps = {
               <div data-aos="fade-right" data-aos-once="true" className="z-[55] flex-col lg:bg-gradient-to-r from-emerald-400 to-cyan-400 lg:flex-row rounded-lg lg:overflow-hidden items-center justify-center	flex w-11/12 h-4/5 lg:w-[840px] lg:h-[555px] bg-cover bg-center">
       <div className="w-full flex justify-center lg:w-2/5 h-full overflow-hidden">
       <Image src={blogConfig.books[count].cover} alt="cover of the book" className={transitionClassImg}/></div>
-<div className="flex h-min lg:h-full bottom-[17px] lg:bottom-[unset] flex-col justify-evenly w-auto rounded-lg lg:rounded-none absolute lg:relative lg:w-3/5 p-5 text-white bg-gradient-to-r lg:bg-none from-emerald-400 to-cyan-400"><div><h3 className="font-inter font-black text-3xl lg:text-5xl">{blogConfig.books[count].title}</h3><p className="jakarta-font">By {blogConfig.books[count].author}</p></div>
-<p className="hidden lg:block jakarta-font !m-0 !lg:mb-[40px] transition">{blogConfig.books[count].description}</p>
+<div className="flex h-min lg:h-full bottom-[17px] lg:bottom-[unset] flex-col justify-evenly w-auto rounded-lg lg:rounded-none absolute lg:relative lg:w-3/5 p-5 text-white bg-gradient-to-r lg:bg-none from-emerald-400 to-cyan-400"><div><h3 className={`font-inter font-black text-3xl lg:text-5xl ${transitionClass}`}>{blogConfig.books[count].title}</h3><p className={`jakarta-font ${transitionClass}`}>By {blogConfig.books[count].author}</p></div>
+<p className={`hidden lg:block jakarta-font !m-0 !lg:mb-[40px] ${transitionClass}`} >{blogConfig.books[count].description}</p>
 </div>
               </div>
           </div>
