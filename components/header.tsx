20ca2b7c5af190eafa7815fe6a3cd4f6caa4b472
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 22:44:16 +0330

    animate

--- components/header.tsx
+++ components/header.tsx
@@ -36,25 +36,6 @@ export function Header() {
           : 'navbar py-8 bg-transparent ',
         'flex flex-row items-center justify-between px-4 xs:px-8 text-darkish dark:text-white',
         'transition-[padding,background-color] duration-300 ease-in-out navbar')}>
-  <div className="navbar-start lg:hidden">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-[30px] h-[30px] lg:h-[25px] lg:w-[25px]" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact !text-base dropdown-content mt-3 p-2 shadow dark:bg-lightdarkish bg-darkerlight rounded-box w-auto">
-      <NavigationBar className="flex-grow flex-column h-auto max-xs:mr-2" />
-    </ul>
-    </div>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu-horizontal px-1">
-    <NavigationBar className="flex-grow max-xs:mr-2" />
-
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <Toolbar className="ml-auto" />
-  </div>
 </div>
 
 
