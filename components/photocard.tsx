7e2cd7344f70a57438e002f600324b53c6558ae7
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 04:01:07 +0330

    animate

--- components/photocard.tsx
+++ components/photocard.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image'
 import placeholder from '../public/images/placeholder.svg'
 import { HeroSection } from '@/components/hero-section';
-type PhotoCardProps = {
-    photo: string;
-  };
   
   export function PhotoCard() {
     
