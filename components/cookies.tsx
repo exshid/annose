0808ab8906f80fbf265094f77d15419cb62e2cda
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-08 02:05:31 +0330

    cookies

--- components/cookies.tsx
+++ components/cookies.tsx
@@ -6,27 +6,26 @@ import React, { useState, useEffect } from 'react';
 import { blogConfig } from '@/config';
 
 const CookieBanner = () => {
-  const [showBanner, setShowBanner] = useState(true);
-  const [acceptedCookies, setAcceptedCookies] = useState(false);
-
-  useEffect(() => {
-    const cookiesAccepted = localStorage.getItem('cookiesAccepted');
-    if (cookiesAccepted) {
+    const [showBanner, setShowBanner] = useState(true);
+    const [acceptedCookies, setAcceptedCookies] = useState(false);
+  
+    useEffect(() => {
+      const cookiesAccepted = localStorage.getItem('cookiesAccepted');
+      if (cookiesAccepted) {
+        setShowBanner(false);
+        setAcceptedCookies(JSON.parse(cookiesAccepted)); 
+      }
+    }, []);
+  
+    const handleAcceptCookies = () => {
+      localStorage.setItem('cookiesAccepted', true);
       setShowBanner(false);
       setAcceptedCookies(true);
+    };
+  
+    if (!showBanner) {
+      return null;
     }
-  }, []);
-
-  const handleAcceptCookies = () => {
-    localStorage.setItem('cookiesAccepted', true);
-    setShowBanner(false);
-    setAcceptedCookies(true);
-  };
-
-  if (!showBanner) {
-    return null;
-  }
-
   return (
     <div className={`cookie-banner w-full fixed bottom-0 ${blogConfig.firstColor}`}>
       <p>We use cookies to improve your experience on our website. <Link className="text-gray" href="/policies">Learn more.</Link></p>
