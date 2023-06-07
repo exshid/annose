37baa460fa8592058af2c9d8d3fce9c055d2efa9
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 19:47:42 +0330

    animate

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -23,4 +23,18 @@ transition={{delay:0.5}}
  )
 
 }
+
+export const ScrollWrapper: React.FC<Props> = ({ children }) => {
+return <motion.div
+  initial={{ opacity: 0, y:150 }}
+  whileInView={{ opacity: 1 }}
+
+  animate={{ opacity: 1, y:0 }}
+transition={{delay:0.5}}
+>
+    {children}
+</motion.div>
+
+
+}
 export default PageWrapper;
