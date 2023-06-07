64ead883bc2b8ad889127e6a4817554137971d77
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2023-06-07 17:54:24 +0330

    animate

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from "react";
 import { MinusSquare, Moon, PlusSquare, Search, Sun, Type } from 'lucide-react';
 import { shallow } from 'zustand/shallow';
-import { useSearchStore } from '@/stores/search-store';
-import { useThemeStore } from '@/stores/theme-store';
 import { Tooltip } from '@/components/tooltip';
 import { cn } from '@/lib/utils';
 
@@ -59,71 +57,6 @@ useEffect(() => {
         className,
       )}
     >
-  {/* 
-{completedProject && (
-      <button onClick={toggleSearch}>
-        <Search
-          id="search"
-          className={cn(
-            'icon-base h-5 w-5 xs:h-6 xs:w-6',
-            isSearching && 'text-accent/60 dark:text-accent-dark/80',
-          )}
-          data-tooltip-content="Search Posts"
-          aria-label="Search Posts"
-        />
-        <Tooltip anchorId="search" />
-      </button>
-)}
-      {fontControls && (
-        <>
-          <button
-            onClick={decreaseFontSize}
-            disabled={isFontSizeMin}
-            className="group"
-          >
-            <MinusSquare
-              id="decrease-font-size"
-              className={cn(
-                'icon-base h-5 w-5 xs:h-6 xs:w-6',
-                'group-disabled:text-accent/60 dark:group-disabled:text-accent-dark/80',
-              )}
-              data-tooltip-content="Decrease font size"
-              aria-label="Decrease font size"
-            />
-            <Tooltip anchorId="decrease-font-size" place="bottom" />
-          </button>
-          <button
-            onClick={increaseFontSize}
-            disabled={isFontSizeMax}
-            className="group"
-          >
-            <PlusSquare
-              id="increase-font-size"
-              className={cn(
-                'icon-base h-5 w-5 xs:h-6 xs:w-6',
-                'group-disabled:text-accent/60 dark:group-disabled:text-accent-dark/80',
-              )}
-              aria-label="Increase font size"
-              data-tooltip-content="Increase font size"
-            />
-            <Tooltip anchorId="increase-font-size" place="bottom" />
-          </button>
-        </>
-      )}
-
-      <button onClick={toggleSerif}>
-        <Type
-          id="serif"
-          className={cn(
-            'icon-base ml-auto h-5 w-5 xs:h-6 xs:w-6',
-            isSerif && 'text-accent/60 dark:text-accent-dark/80',
-          )}
-          data-tooltip-content="Toggle serif font"
-          aria-label="Toggle serif font"
-        />
-        <Tooltip anchorId="serif" place="bottom" />
-      </button>
-  */}
 {darkMode === false && (
 <button
         type="button"
